99f93fc4a8a120d91ce1b3e01e4aa8e556172fc4
heycc <cc.changchun@gmail.com>
2016-01-06 17:54:20 +0800

    gaga

--- proxy/conn.go
+++ proxy/conn.go
@@ -2,163 +2,40 @@ package proxy
 
 import (
 	"bufio"
-	"bytes"
-	"errors"
+	//"bytes"
+	//"errors"
 	"fmt"
 	"io"
 	"net"
-//	"net/url"
-//	"regexp"
 	"strconv"
-	"sync"
+	//"sync"
 	"time"
 )
 
-// conn is the low-level implementation of Conn
 type conn struct {
-
-	// Shared
-	mu      sync.Mutex
-	pending int
-	err     error
 	conn    net.Conn
-
 	// Read
 	readTimeout time.Duration
 	br          *bufio.Reader
-
 	// Write
 	writeTimeout time.Duration
 	bw           *bufio.Writer
 
-	// Scratch space for formatting argument length.
-	// '*' or '$', length, "\r\n"
-	lenScratch [32]byte
-
-	// Scratch space for formatting integers and floats.
-	numScratch [40]byte
+	command		[]byte
+	response	[]byte
 }
 
-
-// NewConn returns a new Redigo connection for the given net connection.
-func NewConn(netConn net.Conn, readTimeout, writeTimeout time.Duration) Conn {
+// NewConn returns a new connection.
+func NewConn(netConn net.Conn, readTimeout, writeTimeout int64) Conn {
 	return &conn{
 		conn:         netConn,
 		bw:           bufio.NewWriter(netConn),
 		br:           bufio.NewReader(netConn),
-		readTimeout:  readTimeout,
-		writeTimeout: writeTimeout,
+		readTimeout:  time.Duration(readTimeout)*time.Millisecond,
+		writeTimeout: time.Duration(readTimeout)*time.Millisecond,
+		command:	make([]byte, 0),
+		response:	make([]byte, 0),
 	}
-}
-
-func (c *conn) Close() error {
-	c.mu.Lock()
-	err := c.err
-	if c.err == nil {
-		c.err = errors.New("redigo: closed")
-		err = c.conn.Close()
-	}
-	c.mu.Unlock()
-	return err
-}
-
-func (c *conn) fatal(err error) error {
-	c.mu.Lock()
-	if c.err == nil {
-		c.err = err
-		// Close connection to force errors on subsequent calls and to unblock
-		// other reader or writer.
-		c.conn.Close()
-	}
-	c.mu.Unlock()
-	return err
-}
-
-func (c *conn) Err() error {
-	c.mu.Lock()
-	err := c.err
-	c.mu.Unlock()
-	return err
-}
-
-func (c *conn) writeLen(prefix byte, n int) error {
-	c.lenScratch[len(c.lenScratch)-1] = '\n'
-	c.lenScratch[len(c.lenScratch)-2] = '\r'
-	i := len(c.lenScratch) - 3
-	for {
-		c.lenScratch[i] = byte('0' + n%10)
-		i -= 1
-		n = n / 10
-		if n == 0 {
-			break
-		}
-	}
-	c.lenScratch[i] = prefix
-	_, err := c.bw.Write(c.lenScratch[i:])
-	return err
-}
-
-func (c *conn) writeString(s string) error {
-	c.writeLen('$', len(s))
-	c.bw.WriteString(s)
-	_, err := c.bw.WriteString("\r\n")
-	return err
-}
-
-func (c *conn) writeBytes(p []byte) error {
-	c.writeLen('$', len(p))
-	c.bw.Write(p)
-	_, err := c.bw.WriteString("\r\n")
-	return err
-}
-
-func (c *conn) writeInt64(n int64) error {
-	return c.writeBytes(strconv.AppendInt(c.numScratch[:0], n, 10))
-}
-
-func (c *conn) writeFloat64(n float64) error {
-	return c.writeBytes(strconv.AppendFloat(c.numScratch[:0], n, 'g', -1, 64))
-}
-
-func (c *conn) writeCommand(cmd string, args []interface{}) (err error) {
-	c.writeLen('*', 1+len(args))
-	err = c.writeString(cmd)
-	for _, arg := range args {
-		if err != nil {
-			break
-		}
-		switch arg := arg.(type) {
-		case string:
-			err = c.writeString(arg)
-		case []byte:
-			err = c.writeBytes(arg)
-		case int:
-			err = c.writeInt64(int64(arg))
-		case int64:
-			err = c.writeInt64(arg)
-		case float64:
-			err = c.writeFloat64(arg)
-		case bool:
-			if arg {
-				err = c.writeString("1")
-			} else {
-				err = c.writeString("0")
-			}
-		case nil:
-			err = c.writeString("")
-		default:
-			var buf bytes.Buffer
-			fmt.Fprint(&buf, arg)
-			err = c.writeBytes(buf.Bytes())
-		}
-	}
-	return err
-}
-
-type protocolError string
-
-func (pe protocolError) Error() string {
-	return fmt.Sprintf("redigo: %s (possible server error or unsupported concurrent read by application)", string(pe))
 }
 
 func (c *conn) readLine() ([]byte, error) {
@@ -173,66 +50,28 @@ func (c *conn) readLine() ([]byte, error) {
 	if i < 0 || p[i] != '\r' {
 		return nil, protocolError("bad response line terminator")
 	}
+	c.bufferResponse(p)
 	return p[:i], nil
 }
 
-// parseLen parses bulk string and array lengths.
-func parseLen(p []byte) (int, error) {
+func (c *conn) bufferResponse(resp []byte) error {
+	// buffer response
+	c.response = append(c.response, resp...)
+	c.response = append(c.response, '\n')
+	fmt.Println(string(c.response))
+	return nil
+}
+
+func parseLen(p []byte) (int64, error) {
 	if len(p) == 0 {
 		return -1, protocolError("malformed length")
 	}
-
-	if p[0] == '-' && len(p) == 2 && p[1] == '1' {
-		// handle $-1 and $-1 null replies.
-		return -1, nil
-	}
-
-	var n int
-	for _, b := range p {
-		n *= 10
-		if b < '0' || b > '9' {
-			return -1, protocolError("illegal bytes in length")
-		}
-		n += int(b - '0')
-	}
-
-	return n, nil
+	return strconv.ParseInt(string(p), 10, 64)
 }
 
-// parseInt parses an integer reply.
-func parseInt(p []byte) (interface{}, error) {
-	if len(p) == 0 {
-		return 0, protocolError("malformed integer")
-	}
-
-	var negate bool
-	if p[0] == '-' {
-		negate = true
-		p = p[1:]
-		if len(p) == 0 {
-			return 0, protocolError("malformed integer")
-		}
-	}
-
-	var n int64
-	for _, b := range p {
-		n *= 10
-		if b < '0' || b > '9' {
-			return 0, protocolError("illegal bytes in length")
-		}
-		n += int64(b - '0')
-	}
-
-	if negate {
-		n = -n
-	}
-	return n, nil
+func parseInt(p []byte) (int64, error) {
+	return strconv.ParseInt(string(p), 10, 64)
 }
-
-var (
-	okReply   interface{} = "OK"
-	pongReply interface{} = "PONG"
-)
 
 func (c *conn) readReply() (interface{}, error) {
 	line, err := c.readLine()
@@ -273,6 +112,7 @@ func (c *conn) readReply() (interface{}, error) {
 		} else if len(line) != 0 {
 			return nil, protocolError("bad bulk string format")
 		}
+		c.bufferResponse(p)
 		return p, nil
 	case '*':
 		n, err := parseLen(line[1:])
@@ -291,104 +131,31 @@ func (c *conn) readReply() (interface{}, error) {
 	return nil, protocolError("unexpected response line")
 }
 
-func (c *conn) Send(cmd string, args ...interface{}) error {
-	c.mu.Lock()
-	c.pending += 1
-	c.mu.Unlock()
-	if c.writeTimeout != 0 {
-		c.conn.SetWriteDeadline(time.Now().Add(c.writeTimeout))
-	}
-	if err := c.writeCommand(cmd, args); err != nil {
-		return c.fatal(err)
-	}
-	return nil
-}
-
-func (c *conn) Flush() error {
-	if c.writeTimeout != 0 {
-		c.conn.SetWriteDeadline(time.Now().Add(c.writeTimeout))
-	}
-	if err := c.bw.Flush(); err != nil {
-		return c.fatal(err)
-	}
-	return nil
-}
-
-func (c *conn) Receive() (reply interface{}, err error) {
-	if c.readTimeout != 0 {
-		c.conn.SetReadDeadline(time.Now().Add(c.readTimeout))
-	}
-	if reply, err = c.readReply(); err != nil {
-		return nil, c.fatal(err)
-	}
-	// When using pub/sub, the number of receives can be greater than the
-	// number of sends. To enable normal use of the connection after
-	// unsubscribing from all channels, we do not decrement pending to a
-	// negative value.
-	//
-	// The pending field is decremented after the reply is read to handle the
-	// case where Receive is called before Send.
-	c.mu.Lock()
-	if c.pending > 0 {
-		c.pending -= 1
-	}
-	c.mu.Unlock()
-	if err, ok := reply.(Error); ok {
-		return nil, err
-	}
-	return
-}
-
 func (c *conn) Do(cmd string, args ...interface{}) (interface{}, error) {
-	c.mu.Lock()
-	pending := c.pending
-	c.pending = 0
-	c.mu.Unlock()
-
-	if cmd == "" && pending == 0 {
-		return nil, nil
-	}
-
 	if c.writeTimeout != 0 {
 		c.conn.SetWriteDeadline(time.Now().Add(c.writeTimeout))
 	}
-
-	if cmd != "" {
-		if err := c.writeCommand(cmd, args); err != nil {
-			return nil, c.fatal(err)
-		}
-	}
-
+	c.bw.Write([]byte(cmd))
 	if err := c.bw.Flush(); err != nil {
-		return nil, c.fatal(err)
+		return nil, protocolError("flush error")
 	}
-
-	if c.readTimeout != 0 {
-		c.conn.SetReadDeadline(time.Now().Add(c.readTimeout))
-	}
-
-	if cmd == "" {
-		reply := make([]interface{}, pending)
-		for i := range reply {
-			r, e := c.readReply()
-			if e != nil {
-				return nil, c.fatal(e)
-			}
-			reply[i] = r
-		}
-		return reply, nil
-	}
-
-	var err error
 	var reply interface{}
-	for i := 0; i <= pending; i++ {
-		var e error
-		if reply, e = c.readReply(); e != nil {
-			return nil, c.fatal(e)
-		}
-		if e, ok := reply.(Error); ok && err == nil {
-			err = e
-		}
+	var e error
+	if reply, e = c.readReply(); e != nil {
+		return nil, protocolError("readReply error")
 	}
-	return reply, err
+	
+	return reply, nil
 }
+
+
+type protocolError string
+
+func (pe protocolError) Error() string {
+	return fmt.Sprintf("proxy: %s", string(pe))
+}
+
+var (
+	okReply   interface{} = "OK"
+	pongReply interface{} = "PONG"
+)
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -4,26 +4,16 @@ type Error string
 
 func (err Error) Error() string { return string(err) }
 
-// Conn represents a connection to a Redis server.
 type Conn interface {
-	// Close closes the connection.
-	Close() error
+	//Close() error
 
-	// Err returns a non-nil value if the connection is broken. The returned
-	// value is either the first non-nil value returned from the underlying
-	// network connection or a protocol parsing error. Applications should
-	// close broken connections.
-	Err() error
+	Do(cmd string, args ...interface{}) (reply interface{}, err error)
 
-	// Do sends a command to the server and returns the received reply.
-	Do(commandName string, args ...interface{}) (reply interface{}, err error)
-
-	// Send writes the command to the client's output buffer.
-	Send(commandName string, args ...interface{}) error
+	//Send(commandName string, args ...interface{}) error
 
 	// Flush flushes the output buffer to the Redis server.
-	Flush() error
+	//Flush() error
 
 	// Receive receives a single reply from the Redis server
-	Receive() (reply interface{}, err error)
+	//Receive() (reply interface{}, err error)
 }
